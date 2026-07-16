15cc50cd1c21023d4890c7308203fe1094913728
Bunnarin Chrun <chrunbunnarin857@gmail.com>
2026-07-16 11:23:19 +0700

    fixed appending scholarshipSource

--- src/routes/student/[id]/+page.js
+++ src/routes/student/[id]/+page.js
@@ -3,21 +3,18 @@ import { PUBLIC_PORTAL_API_KEY } from "$env/static/public";
 export const load = async ({ params }) => {
     const query = new URLSearchParams({
         filterByTk: params.id,
-        appends: 'enrollments,enrollments.program,picture,scholarshipSource,user,classes'
+        appends: 'enrollments,enrollments.program,picture,user,classes'
     });
-    const res = await fetch(`https://portal.rua.edu.kh/api/student:get?${query}`, {
+    const res = await fetch(`https://rua.popok.uk/api/student:get?${query}`, {
         headers: {
             "Authorization": `Bearer ${PUBLIC_PORTAL_API_KEY}`
         },
     });
     const { data } = await res.json();
     // inject our own
-    if (data.scholarshipSourceId)
-        data.scholarship = data.scholarshipSource?.name + `(${data.scholarshipCoverage}%)`;
     data.phone = data.user?.phone;
-    data.email = data.user?.email;
     if (data.picture)
-        data.photo = 'https://portal.rua.edu.kh' + data.picture.url;
+        data.photo = 'https://rua.popok.uk' + data.picture.url;
     data.classes = data.classes.map((c) => c.name).join(', ');
     const fieldsToHide = ['highschoolType', 'bacIIYear', 'bacIIGrade', 'noNationalIdYet', 'bacIICertificateID', 'nationalID', 'bacIIGrade'];
     // bassically hide anything that ends with 'Id'
